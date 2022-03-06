073c6660a71b6533e689c90449d0b5ae3967bc57
HsudongDai <670666751@qq.com>
2022-03-05 22:27:00 -0500

    1

--- filecopy/netutility.cpp
+++ filecopy/netutility.cpp
@@ -33,7 +33,7 @@ namespace C150NETWORK {
     // send a message
     // return the response
     Packet arrayToPacket(vector<char>& recBuffer) {
-        char lenBuffer[4];
+        unsigned char lenBuffer[4];
         // get the message type
         memcpy(lenBuffer, recBuffer.data(), 4);
         int messageType = charArrayToInt(lenBuffer);
