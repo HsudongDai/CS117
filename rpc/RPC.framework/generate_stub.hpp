69b2aff4ecad92d486eeb7b02c57cf75ef2bf47e
HsudongDai <670666751@qq.com>
2022-03-31 16:39:23 -0400

    Add a separate header file

---
+++ rpc/RPC.framework/generate_stub.hpp
@@ -0,0 +1,27 @@
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <map>
+#include "c150exceptions.h"
+#include "declarations.h"
+#include "functiondeclaration.h"
+#include "typedeclaration.h"
+
+#include "idl_to_json.cpp"
+
+using namespace std;
+using namespace C150NETWORK;
+
+namespace C150NETWORK{
+    int writeStubHeader(stringstream& output, const char idl_filename[]);
+    int writeStructDefinitions(stringstream& output, const Declarations& parseTree);
+    int writeStubFunctions(stringstream& output, const Declarations& parseTree);
+    int writeStubBadFunction(stringstream& output, const char idl_filename[]);
+    int writeStubDispatcher(stringstream& output, const Declarations& parseTree);
+    int writeStubGetFunctionNameFromStream(stringstream& output, const char idl_filename[]);
+    int generateStub(const char idl_filename[]);
+}
