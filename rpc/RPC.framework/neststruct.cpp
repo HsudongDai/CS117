cb228daf47c055ccd921f01f6820a2b54d6f96fa
HsudongDai <670666751@qq.com>
2022-04-11 21:26:50 -0400

    Add another test

---
+++ rpc/RPC.framework/neststruct.cpp
@@ -0,0 +1,11 @@
+#include "neststruct.idl"
+
+using namespace std;
+
+Person findPerson(ThreePeople tp) {
+    return tp.p1;
+}
+
+rectangle searchRectangles(rectangle rects[10]) {
+    return rects[0];
+}
